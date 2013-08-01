a85b11c72a418784cbda12780602e8c32bd3dede
Marc Paradise <marc@opscode.com>
2013-07-31 23:04:15 -0400

    temporary fix to use priv path to start executable

--- src/depselector_sup.erl
+++ src/depselector_sup.erl
@@ -11,9 +11,20 @@ start_link() ->
     supervisor:start_link(depselector_sup, []).
 
 init([]) ->
-    PrivDir = code:priv_dir(depsolver_app),
+    PrivDir = priv_dir(),
     ExePath = filename:join([PrivDir, "solver"]),
     {ok, {{one_for_one, 3, 10},
           [{depselector, {depselector, start_link, [ExePath]},
             permanent, 10, worker, [depselector]}]}}.
 
+priv_dir() ->
+    priv_dir(code:priv_dir(depsolver)).
+
+% TODO this is a hack that I strongly suspect won't work when depsolver
+% itself is a dep ...
+priv_dir({error, bad_name}) ->
+    ModDir = filename:dirname(code:which(depsolver)),
+    BaseDir = filename:dirname(ModDir),
+    filename:join(BaseDir, "priv");
+priv_dir(Other) ->
+    Other.
