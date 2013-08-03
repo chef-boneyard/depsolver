dc0ccdd7962cafc741850ff9c94d3b0480fa98d6
Mark Anderson <mark@opscode.com>
2013-08-03 00:23:50 -0700

    Translation layer for depselector; messy code in depsolver_gecode.erl needs cleanup, and there's some interface layer work.

---
+++ src/version_manager.erl
@@ -0,0 +1,308 @@
+%%% -*- erlang-indent-level: 4; indent-tabs-mode: nil; fill-column: 100 -*-
+%%% ex: ts=4 sx=4 et
+%%%-------------------------------------------------------------------
+%%% @author Mark Anderson <>
+%%% @copyright (C) 2013, Mark Anderson
+%%% @doc
+%%%
+%%% @end
+%%% Created : 30 Jul 2013 by Mark Anderson <>
+%%%-------------------------------------------------------------------
+-module(version_manager).
+-include_lib("eunit/include/eunit.hrl").
+%% API
+-export([parse/1,
+         new/0,
+         make/1, %% deprecated
+         add_package/3,
+         get_id/2,
+         get_version_id/3,
+         map_constraint/3,
+         unmap_constraint/2,
+         int_to_version/2,
+         version_to_int/2,
+         constraint_to_range/2,
+         search_eq/2,
+         search_gt/2,
+         search_gte/2,
+         search_lt/2,
+         search_lte/2]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% Valid package versions are between -1 and its max (-1 means don't care, meaning it doesn't need
+%% to be assigned). To indicate constraints that match no versions, -2 is used, since it's not a
+%% valid assignment of the variable; thus, any branch that assigns -2 will fail.
+%%
+%% This mechanism is also used when a dependent package has no versions, which only happens if the
+%% dependency's package is auto-vivified when creating the parent PackageVersion's dependency but
+%% with no corresponding set of PackageVersions (i.e. it's an invalid deendency, because it does not
+%% exist in the dependency graph). Again, we won't abort immediately, but we'll add a constraint to
+%% the package that makes exploring that portion of the solution space unsatisfiable. Thus it is
+%% impossible to find solutions dependent on non-existent packages.
+-define(DONT_CARE_CONSTRAINT, -1).
+-define(NO_MATCH_CONSTRAINT, {-2, -2}).
+
+-type vsn() :: 'NO_VSN'
+             | ec_semver:semver().
+
+-type constraint_op() ::
+        '=' | gte | '>=' | lte | '<='
+      | gt | '>' | lt | '<' | pes | '~>' | between.
+
+-type constraint() :: {vsn(), constraint_op()}.
+
+%-type vsn_constraint() :: {raw_vsn(), [raw_constraint()]}.
+%-type dependency_set() :: {pkg_name(), [vsn_constraint()]}.
+
+-record(cookbook_version_mapper,
+        { versionList :: array } ).
+
+-record(package,
+        { name :: string(),
+          index :: integer(),
+          versionMapper :: any() }). %% cookbook_version_mapper{} }).
+
+
+-record(problem,
+        { byName :: gb_tree(),
+          byId   :: array()} ).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+
+new() ->
+    #problem{byName = gb_trees:empty(), byId = array:new()}.
+
+add_package(PkgName, Versions, #problem{byName=ByName, byId=ById} = Prob) ->
+    CVM = make(Versions),
+    Index = array:size(ById),
+    Package = #package{name = PkgName, index = Index, versionMapper = CVM},
+    NById = array:set(Index, Package, ById),
+    NByName = gb_trees:enter(PkgName, Package, ByName),
+    Prob#problem{byName=NByName, byId = NById}.
+
+get_id(PkgName, #problem{byName=ByName}) ->
+    {value, #package{index=Index}} = gb_trees:lookup(PkgName, ByName),
+    Index.
+
+get_version_id(PkgName, Version, #problem{byName=ByName}) ->
+    {value, #package{index=Index, versionMapper = Mapper}} = gb_trees:lookup(PkgName, ByName),
+    VersionId = version_to_int(Version, Mapper),
+    {Index, VersionId}.
+
+map_constraint(DependentPackage, Constraint, #problem{byName=ByName}) ->
+    case gb_trees:lookup(DependentPackage, ByName) of
+        none ->
+            %% TODO Check that this is correct (re-read depselector code to discover what we do when
+            %% there is a dependency on a missing constraint; I suspect there we need to flag it as
+            %% 'suspicious'...
+            no_matching_package;
+        {value, #package{index=Index, versionMapper = Mapper}} ->
+            ?debugVal(Constraint),
+            ConstraintRange = constraint_to_range(Constraint, Mapper),
+            {Index, ConstraintRange}
+    end.
+
+unmap_constraint({PackageId, VersionId}, #problem{byId=ById}) ->
+    #package{name = PackageName, versionMapper = VersionMapper} = array:get(PackageId, ById),
+    Version = int_to_version(VersionId, VersionMapper),
+    {PackageName, Version}.
+
+
+%% Version mapping/unmapping to a dense set
+-spec make([vsn()]) -> #cookbook_version_mapper{}.
+make(Versions) ->
+    SortedVersions = lists:sort(fun ec_semver:lt/2, lists:map(fun parse/1, Versions)),
+    ArrayVersions = array:from_list(SortedVersions),
+    #cookbook_version_mapper{versionList = ArrayVersions}.
+
+version_to_int(Version, #cookbook_version_mapper{versionList = VersionList}) ->
+    search_eq(Version, VersionList).
+
+int_to_version(Value, #cookbook_version_mapper{versionList = Versions}) ->
+    {Version, _} = array:get(Value, Versions),
+    Version.
+
+parse({X,Y,Z}) when not is_tuple(X) ->
+    {{X,Y,Z}, {[],[]}};
+parse({X,Y}) when not is_tuple(X) ->
+    {{X,Y}, {[],[]}};
+parse({X}) when not is_tuple(X) ->
+    {{X}, {[],[]}};
+parse(X) ->
+    ec_semver:parse(X).
+
+
+%% Manage folding a version tuple down to
+
+-spec constraint_to_range(constraint(), #cookbook_version_mapper{}) -> {int, int}.
+constraint_to_range(any, #cookbook_version_mapper{versionList = VersionList}) ->
+    Max = find_max(VersionList),
+    maybe_impossible_constraint(0, Max);
+
+constraint_to_range({Version, '='}, Versions) ->
+    constraint_to_range({Version, eq}, Versions);
+constraint_to_range({Version, eq},   #cookbook_version_mapper{versionList = VersionList}) ->
+    Min = search_eq(Version, VersionList),
+    maybe_impossible_constraint(Min,Min);
+
+constraint_to_range({Version, '>='}, Versions) ->
+    constraint_to_range({Version, gte}, Versions);
+constraint_to_range({Version, gte},   #cookbook_version_mapper{versionList = VersionList}) ->
+    Min = search_gte(Version, VersionList),
+    Max = find_max(VersionList),
+    maybe_impossible_constraint(Min, Max);
+
+constraint_to_range({Version, '>'}, Versions) ->
+    constraint_to_range({Version, gt}, Versions);
+constraint_to_range({Version, gt},   #cookbook_version_mapper{versionList = VersionList}) ->
+    Min = search_gt(Version, VersionList),
+    Max = find_max(VersionList),
+    maybe_impossible_constraint(Min, Max);
+
+constraint_to_range({Version, '<='}, Versions) ->
+    constraint_to_range({Version, lte}, Versions);
+constraint_to_range({Version, lte},  #cookbook_version_mapper{versionList = VersionList}) ->
+    Max = search_lte(Version, VersionList),
+    maybe_impossible_constraint(0, Max);
+
+constraint_to_range({Version, '<'}, Versions) ->
+    constraint_to_range({Version, lt}, Versions);
+constraint_to_range({Version, lt},  #cookbook_version_mapper{versionList = VersionList}) ->
+    Max = search_lt(Version, VersionList),
+    maybe_impossible_constraint(0, Max);
+
+constraint_to_range({Version, '~>'}, Versions) ->
+    constraint_to_range({Version, pes}, Versions);
+constraint_to_range({Version, pes},  #cookbook_version_mapper{versionList = VersionList}) ->
+    Min = search_gte(normalize(Version), VersionList),
+    Max = search_lt(find_next_version(Version), VersionList),
+    maybe_impossible_constraint(Min,Max).
+
+maybe_impossible_constraint(not_found, _)->
+    ?NO_MATCH_CONSTRAINT;
+maybe_impossible_constraint(_, not_found) ->
+    ?NO_MATCH_CONSTRAINT;
+maybe_impossible_constraint(Min, Max) when Min > Max ->
+    ?NO_MATCH_CONSTRAINT;
+maybe_impossible_constraint(Min, Max) ->
+    {Min, Max}.
+
+find_max(#cookbook_version_mapper{versionList = VersionList}) ->
+    find_max(VersionList);
+find_max(VersionList) ->
+    array:size(VersionList)-1.
+
+normalize({Major}) when not is_tuple(Major) ->
+    {Major, 0, 0};
+normalize({Major, Minor}) when not is_tuple(Major) ->
+    {Major, Minor, 0};
+normalize({Major, Minor, Patch}) when not is_tuple(Major) ->
+    {Major, Minor, Patch}.
+
+find_next_version({Major}) ->
+    {Major+1, 0, 0};
+find_next_version({Major, Minor}) ->
+    {Major, Minor+1, 0};
+find_next_version({Major, Minor, Patch}) ->
+    {Major, Minor, Patch+1}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+search_eq(Version, VersionArray) ->
+    NVersion = parse(Version),
+    End = array:size(VersionArray),
+    Pos = search_gte(VersionArray, NVersion, 0, End),
+    case Pos of
+        End -> not_found;
+        _ -> %% In bounds, safe to fetch
+            FVersion = array:get(Pos, VersionArray),
+            case ec_semver:eql(FVersion, NVersion) of
+                true -> Pos;
+                _ -> not_found
+            end
+    end.
+
+%% Binary search: Find least element greater than or equal to Version
+search_gte(Version, VersionArray) ->
+    NVersion = parse(Version),
+    End = array:size(VersionArray),
+    Pos = search_gte(VersionArray, NVersion, 0, End),
+    case Pos of
+        End ->
+            not_found;
+        _ -> 
+            Pos
+    end.
+
+%% Binary search: Find least element greater than Version
+search_gt(Version, VersionArray) ->
+    NVersion = parse(Version),
+    End = array:size(VersionArray),
+    Pos = search_gte(VersionArray, NVersion, 0, End),
+    case Pos of
+        End ->
+            not_found;
+        _ -> %% In bounds, safe to fetch
+            FVersion = array:get(Pos, VersionArray),
+            case ec_semver:eql(FVersion, NVersion) of
+                true when Pos =:= (End-1) ->
+                    not_found;
+                true -> Pos+1;
+                _ -> Pos
+            end
+    end.
+
+%% Binary search: Find greatest element less than or equal to Version
+search_lte(Version, VersionArray) ->
+    NVersion = parse(Version),
+    End = array:size(VersionArray),
+    %% This returns the least element greater than or equal to version..
+    Pos = search_gte(VersionArray, NVersion, 0, End),
+    case Pos < End andalso ec_semver:eql(array:get(Pos, VersionArray), NVersion) of
+        true ->
+            Pos;
+        _ when Pos =:= 0 ->
+            not_found;
+        _ ->
+            Pos - 1
+    end.
+
+%% Binary search: Find greatest element less than Version
+search_lt(Version, VersionArray) ->
+    NVersion = parse(Version),
+    End = array:size(VersionArray),
+    %% This returns the least element greater than or equal to version..
+    Pos = search_gte(VersionArray, NVersion, 0, End),
+    case Pos of
+        0 ->
+            not_found;
+        _ ->
+            Pos - 1
+    end.
+
+%% Binary search: Find least element greater than or equal to Version
+%% Start start of array
+%% End end+1 of array (Valid range Start<= x < End)
+%% Return value Pos is in [Start, End] (inclusive)
+search_gte(_VersionArray, _Version, Pos, Pos) ->
+    Pos;
+search_gte(VersionArray, Version, Start, End) ->
+    Pos = trunc((Start+End)/2), %% Pos < End if Start < End
+    case ec_semver:gte(array:get(Pos, VersionArray), Version) of
+        true ->
+            %% Start <= Pos < End, so this always shrinks the bounds, maintains invariant Start<End
+            search_gte(VersionArray, Version, Start, Pos);
+        _ ->
+            %% Start < End so this shrinks bounds, but does not break invariant Start<=End
+            search_gte(VersionArray, Version, Pos+1, End)
+    end.
---
+++ test/version_manager_tests.erl
@@ -0,0 +1,275 @@
+%% -*- erlang-indent-level: 4; indent-tabs-mode: nil; fill-column: 80 -*-
+%% ex: ts=4 sw=4 et
+%%
+%%-------------------------------------------------------------------
+%% Copyright 2013 Opscode, Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% @author Mark Anderson <mark@opscode.com>
+%%-------------------------------------------------------------------
+-module(version_manager_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-compile([export_all]).
+
+-define(NO_MATCH_CONSTRAINT, {-2,-2}).
+
+-define(VERSION_SET_SIMPLE2, [ {0,1,0}, %% This is deliberately out of order
+                              {0,2},
+                              {3,0},
+                              {0,0,1},
+                              {1,0},
+                              {0,1,3},
+                              {2,0,0},
+                              {4,0,0},
+                              {5,0,0},
+                              {6,0,0}] ).
+-define(VERSION_SET_PARSED, [{0,1,0}, %% This is deliberately out of order
+                             {0,2,0},
+                             {3,0,0},
+                             {0,0,1},
+                             {1,0,0},
+                             {0,1,3},
+                             {2,0,0},
+                             {4,0,0},
+                             {5,0,0},
+                             {6,0,0}] ).
+
+
+-define(PROBLEM,  [{app1, ["0.1",
+                           "0.2",
+                           "0.3"]},
+                   {app2, ["0.1",
+                           "0.2",
+                           "0.3"]},
+                   {app3, ["0.1",
+                           "0.2",
+                           "0.3"]} ]).
+
+version_map_test_() ->
+    {foreach,
+     fun() ->
+             ok
+     end,
+     fun(_) ->
+             ok
+     end,
+     [
+      {"Simple init and fetch of versions comes out in order",
+       fun() ->
+               Versions = version_manager:make(?VERSION_SET_PARSED),
+               ?assertEqual({0,0,1}, version_manager:int_to_version(0, Versions)),
+               ?assertEqual({0,1,0}, version_manager:int_to_version(1, Versions)),
+               ?assertEqual({0,1,3}, version_manager:int_to_version(2, Versions)),
+               ?assertEqual({0,2,0}, version_manager:int_to_version(3, Versions)),
+               ?assertEqual({1,0,0}, version_manager:int_to_version(4, Versions)),
+               ?assertEqual({2,0,0}, version_manager:int_to_version(5, Versions)),
+               ?assertEqual({3,0,0}, version_manager:int_to_version(6, Versions)),
+               ?assertEqual({4,0,0}, version_manager:int_to_version(7, Versions)),
+               ?assertEqual({5,0,0}, version_manager:int_to_version(8, Versions)),
+               ?assertEqual({6,0,0}, version_manager:int_to_version(9, Versions))
+       end}
+     ]}.
+
+search_test() ->
+    {foreach,
+     fun() ->
+             %% Quick hack to extract from record
+             element(2,version_manager:make(?VERSION_SET_PARSED))
+     end,
+     fun(_) ->
+             ok
+     end,
+     [
+      fun(A) ->
+              [
+               {"Equality search works",
+                fun() ->
+                        ?assertEqual(not_found, version_manager:search_eq({0,0,0}, A)),
+                        ?assertEqual(0, version_manager:search_eq({0,0,1}, A)),
+                        ?assertEqual(4, version_manager:search_eq({1,0,0}, A)),
+                        ?assertEqual(not_found, version_manager:search_eq({1,0,1}, A)),
+                        ?assertEqual(9, version_manager:search_eq({6,0,0}, A))
+                        ?assertEqual(not_found, version_manager:search_eq({6,0,1}, A))
+                end},
+               {"less_than search works",
+                fun() ->
+                        ?assertEqual(not_found, version_manager:search_lt({0,0,0}, A)),
+                        ?assertEqual(not_found, version_manager:search_lt({0,0,1}, A)),
+                        ?assertEqual(3, version_manager:search_lt({1,0,0}, A)),
+                        ?assertEqual(4, version_manager:search_lt({1,0,1}, A)),
+                        ?assertEqual(8, version_manager:search_lt({6,0,0}, A))
+                        ?assertEqual(9, version_manager:search_lt({6,0,1}, A))
+                end},
+               {"less_than_equals search works",
+                fun() ->
+                        ?assertEqual(not_found, version_manager:search_lte({0,0,0}, A)),
+                        ?assertEqual(0, version_manager:search_lte({0,0,1}, A)),
+                        ?assertEqual(4, version_manager:search_lte({1,0,0}, A)),
+                        ?assertEqual(5, version_manager:search_lte({1,0,1}, A)),
+                        ?assertEqual(9, version_manager:search_lte({6,0,0}, A))
+                        ?assertEqual(9, version_manager:search_lte({6,0,1}, A))
+                end},
+               {"greater_than_equals search works",
+                fun() ->
+                        ?assertEqual(0, version_manager:search_gte({0,0,0}, A)),
+                        ?assertEqual(0, version_manager:search_gte({0,0,1}, A)),
+                        ?assertEqual(4, version_manager:search_gte({1,0,0}, A)),
+                        ?assertEqual(5, version_manager:search_gte({1,0,1}, A)),
+                        ?assertEqual(9, version_manager:search_gte({6,0,0}, A))
+                        ?assertEqual(not_found, version_manager:search_gte({6,0,1}, A))
+                end},
+               {"greater_than search works",
+                fun() ->
+                        ?assertEqual(0, version_manager:search_lte({0,0,0}, A)),
+                        ?assertEqual(1, version_manager:search_gt({0,0,1}, A)),
+                        ?assertEqual(5, version_manager:search_gt({1,0,0}, A)),
+                        ?assertEqual(5, version_manager:search_gt({1,0,1}, A)),
+                        ?assertEqual(not_found, version_manager:search_gt({6,0,0}, A))
+                        ?assertEqual(not_found, version_manager:search_gt({6,0,1}, A))
+                end}
+              ]
+      end]}.
+
+constraint_map_test_() ->
+    {foreach,
+     fun() ->
+             version_manager:make(?VERSION_SET_PARSED)
+     end,
+     fun(_) ->
+             ok
+     end,
+     [
+      fun(V) ->
+              [
+               {"Equality constraints make sense",
+                fun() ->
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{0,0,0}, eq}, V)),
+                        ?assertEqual({0,0}, version_manager:constraint_to_range({{0,0,1}, eq}, V)),
+                        ?assertEqual({4,4}, version_manager:constraint_to_range({{1,0,0}, eq}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{1,0,1}, eq}, V)),
+                        ?assertEqual({9,9}, version_manager:constraint_to_range({{6,0,0}, eq}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{6,0,1}, eq}, V))
+                end},
+               {"LTE constraints make sense",
+                fun() ->
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{0,0,0}, lte}, V)),
+                        ?assertEqual({0,0}, version_manager:constraint_to_range({{0,0,1}, lte}, V)),
+                        ?assertEqual({0,4}, version_manager:constraint_to_range({{1,0,0}, lte}, V)),
+                        ?assertEqual({0,4}, version_manager:constraint_to_range({{1,0,1}, lte}, V)),
+                        ?assertEqual({0,9}, version_manager:constraint_to_range({{6,0,0}, lte}, V)),
+                        ?assertEqual({0,9}, version_manager:constraint_to_range({{6,0,1}, lte}, V))
+                end},
+               {"LT constraints make sense",
+                fun() ->
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{0,0,0}, lt}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{0,0,1}, lt}, V)),
+                        ?assertEqual({0,0}, version_manager:constraint_to_range({{0,1,0}, lt}, V)),
+                        ?assertEqual({0,3}, version_manager:constraint_to_range({{1,0,0}, lt}, V)),
+                        ?assertEqual({0,4}, version_manager:constraint_to_range({{1,0,1}, lt}, V)),
+                        ?assertEqual({0,8}, version_manager:constraint_to_range({{6,0,0}, lt}, V)),
+                        ?assertEqual({0,9}, version_manager:constraint_to_range({{6,0,1}, lt}, V))
+                end},
+              {"GTE constraints make sense",
+                fun() ->
+                        ?assertEqual({0,9}, version_manager:constraint_to_range({{0,0,0}, gte}, V)),
+                        ?assertEqual({0,9}, version_manager:constraint_to_range({{0,0,1}, gte}, V)),
+                        ?assertEqual({4,9}, version_manager:constraint_to_range({{1,0,0}, gte}, V)),
+                        ?assertEqual({5,9}, version_manager:constraint_to_range({{1,0,1}, gte}, V)),
+                        ?assertEqual({9,9}, version_manager:constraint_to_range({{6,0,0}, gte}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{6,0,1}, gte}, V))
+                end},
+               {"GT constraints make sense",
+                fun() ->
+                        ?assertEqual({0,9}, version_manager:constraint_to_range({{0,0,0}, gt}, V)),
+                        ?assertEqual({1,9}, version_manager:constraint_to_range({{0,0,1}, gt}, V)),
+                        ?assertEqual({5,9}, version_manager:constraint_to_range({{1,0,0}, gt}, V)),
+                        ?assertEqual({5,9}, version_manager:constraint_to_range({{1,0,1}, gt}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{6,0,0}, gt}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{6,0,1}, gt}, V))
+                end},
+               {"PES (~>) constraints make sense",
+                fun() ->
+                        ?assertEqual(?NO_MATCH_CONSTRAINT, version_manager:constraint_to_range({{0,0,0}, pes}, V)),
+                        ?assertEqual(lo({0,0,1}, {0,0,1}, V),
+                                     version_manager:constraint_to_range({{0,0}, pes}, V)),
+                        ?assertEqual(lo({0,0,1}, {0,2,0}, V),
+                                     version_manager:constraint_to_range({{0}, pes}, V)),
+                        ?assertEqual(lo({0,0,1}, {0,0,1}, V),
+                                     version_manager:constraint_to_range({{0,0,1}, pes}, V)),
+                        ?assertEqual(lo({0,1,0}, {0,1,3}, V),
+                                     version_manager:constraint_to_range({{0,1}, pes}, V)),
+                        ?assertEqual(lo({0,2,0}, {0,2,0}, V),
+                                     version_manager:constraint_to_range({{0,2,0}, pes}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT,
+                                     version_manager:constraint_to_range({{0,2,1}, pes}, V)),
+                        ?assertEqual(lo({1,0,0}, {1,0,0}, V),
+                                     version_manager:constraint_to_range({{1,0}, pes}, V)),
+                        ?assertEqual(lo({1,0,0}, {1,0,0}, V),
+                                     version_manager:constraint_to_range({{1,0,0}, pes}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT,
+                                     version_manager:constraint_to_range({{1,0,1}, pes}, V)),
+                        ?assertEqual(lo({6,0,0}, {6,0,0}, V),
+                                     version_manager:constraint_to_range({{6,0,0}, pes}, V)),
+                        ?assertEqual(?NO_MATCH_CONSTRAINT,
+                                     version_manager:constraint_to_range({{6,0,1}, pes}, V))
+                end}
+              ]
+      end
+     ]}.
+
+version_manager_make() ->
+    version_manager:make(?VERSION_SET_PARSED).
+
+lo(V,A) when element(1,A) =:= cookbook_version_mapper ->
+    version_manager:search_eq(V,element(2,A));
+lo(V,A) ->
+    version_manager:search_eq(V,A).
+
+lo(V1,V2,A) ->
+    {lo(V1,A), lo(V2, A)}.
+
+problem_map_test_() ->
+    {foreach,
+     fun() ->
+             ok
+     end,
+     fun(_) ->
+             ok
+     end,
+     [
+      {"Simple init and fetch of apps comes out in order",
+       fun() ->
+               Problem = make_problem(),
+               ?assertEqual(0, version_manager:get_id(app1,Problem)),
+               ?assertEqual(1, version_manager:get_id(app2,Problem)),
+               ?assertEqual(2, version_manager:get_id(app3,Problem))
+       end},
+      {"Simple init and fetch of apps, versions comes correct",
+       fun() ->
+               Problem = make_problem(),
+               ?assertEqual({0,0}, version_manager:get_version_id(app1, "0.1",Problem)),
+               ?assertEqual({1,0}, version_manager:get_version_id(app2, "0.1",Problem)),
+               ?assertEqual({2,1}, version_manager:get_version_id(app3, "0.2",Problem)),
+               ?assertEqual({2,2}, version_manager:get_version_id(app3, "0.3",Problem))
+       end}
+
+     ]}.
+
+make_problem() ->
+    Problem = version_manager:new(),
+    lists:foldl(fun({N,V},A) -> version_manager:add_package(N,V,A) end,
+                           Problem, ?PROBLEM).
