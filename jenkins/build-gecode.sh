9a576e991c590a63b804f7f475082ad583ff82aa
Ho-Sheng Hsiao <hosh@opscode.com>
2013-08-13 10:33:05 -0700

    Use a different name for the gecode deb
    opscode-chef and Ruby depsolver still needs system gecode. Change the
    name of the deb package so that we can have both system gecode and the
    custom gecode. Erl depsolver should still compile against the custom
    gecode in /opt/gecode

--- jenkins/build-gecode.sh
+++ jenkins/build-gecode.sh
@@ -9,6 +9,10 @@ GECODE_VERSION="3.7.3"
 GECODE_SRC_URL="http://www.gecode.org/download/gecode-${GECODE_VERSION}.tar.gz"
 GECODE_SRC="gecode-${GECODE_VERSION}.tar.gz"
 
+# Use a different name for the package so we can keep the system libgecode
+# Remove this when opscode-chef and Ruby depselector are no longer needed
+GECODE_DEB_NAME="gecode-opscode"
+
 # Define FAKE_ROOT so that fpm can package this up as if it were located in /opt/gecode rather
 # than a temporary (but writable) workspace.
 FAKE_ROOT=${WORKSPACE}/fake_root/
@@ -55,7 +59,7 @@ rm -f ${GECODE_DEB}
 
 # Make deb package. Point FPM to the fake root and only package up opt/gecode
 # The resulting deb will install to /opt/gecode
-fpm -s dir -t deb -n ${PROJ_NAME} -v ${GECODE_VERSION} -C ${FAKE_ROOT} \
+fpm -s dir -t deb -n ${GECODE_DEB_NAME} -v ${GECODE_VERSION} -C ${FAKE_ROOT} \
   -p ${GECODE_DEB} \
   opt/gecode
 
